20faf13b3d8bfd00cecda4e81681288fde99a574
Aleksi Peebles <aleksi@iki.fi>
2016-03-15 17:54:07 +0200

    Add development files moved from Shila theme

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # Shila Drupal Theme StarterKit
 
-This StarterKit is for the [Twig Edition of Pattern Lab](https://github.com/pattern-lab/edition-php-twig-standard) and is meant to be used with Drupal 8 themes based on the [Shila theme](https://github.com/aleksip/shila-drupal-theme).
+A StarterKit for the [Twig Edition of Pattern Lab](https://github.com/pattern-lab/edition-php-twig-standard). Used in developing Drupal 8 themes and Pattern Lab style guides based on [Shila theme](https://github.com/aleksip/shila-drupal-theme). At the moment a forked version of Pattern Lab is required for Drupal template compatibility.
 
-In early stages of development.
+Tries to follow [Drupal CSS coding standards](https://www.drupal.org/coding-standards/css).
+
+A Pattern Lab generated style guide can be found [here](https://aleksip.github.io/styleguide-shila-drupal-theme/) (online style guide might not be up to date).
+
+Please note that this StarterKit is in early stages of development.
+
+
+## Development tools, libraries and frameworks used
+
+- [libSass](http://sass-lang.com/libsass) Sass engine
+- [Singularity](http://singularity.gs/) grid framework
+- [Bourbon](http://bourbon.io/) mixin library
+- [Bower](http://bower.io/) package manager
+- [Gulp](http://gulpjs.com/) toolkit
+
+
+## Installing development dependencies
+
+Prerequisites: [npm](https://nodejs.org/) and [Bower](http://bower.io/) installed.
+
+In the StarterKit root directory run:
+ 
+```sh
+npm install
+bower install
+```
+
+
+## Integrating with Shila theme and Pattern Lab
+
+Please see [Shila theme](https://github.com/aleksip/shila-drupal-theme) documentation.
---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var config      = require('./config.json');
+var gulp        = require('gulp');
+var run         = require('gulp-run');
+var runSequence = require('run-sequence');
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var fs          = require('fs');
+
+function isDirectory(dir) {
+    try {
+        return fs.statSync(dir).isDirectory();
+    }
+    catch (err) {
+        return false;
+    }
+}
+
+gulp.task('watch', ['sass-change'], function() {
+    browserSync.init({
+        proxy: {
+            target: "localhost:8080",
+            reqHeaders: function(config) {
+                return {
+                    "host": "www.aleksip.dev"
+                }
+            }
+        }
+    });
+    gulp.watch(config.sass.srcFiles, ['sass-change']);
+    gulp.watch(config.patternFiles, ['patterns-change']);
+    gulp.watch(config.imageFiles, ['drupal:copy-images']);
+    if (isDirectory(config.drupal.templatesDir)) {
+        gulp.watch(
+            config.drupal.templatesDir + '/**/*.html.twig',
+            ['templates-change']
+        );
+    }
+});
+
+gulp.task('sass', function() {
+    return gulp.src(config.sass.srcFiles)
+        .pipe(sourcemaps.init())
+        .pipe(sass(config.sass.options).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.sass.destDir))
+    ;
+});
+
+gulp.task('sass-change', function() {
+    runSequence('sass', 'drupal:copy-css', 'pl:generate');
+});
+
+gulp.task('patterns-change', function() {
+    runSequence('drupal:copy-patterns', 'pl:generate');
+});
+
+gulp.task('templates-change', function() {
+    runSequence('drush:cr', 'bs:reload');
+});
+
+gulp.task('drupal:copy-css', function() {
+    if (isDirectory(config.drupal.cssDir)) {
+        gulp.src([
+                config.sass.destDir + '/**/*',
+                '!' + config.sass.destDir + '/hidden.module.css',
+                '!' + config.sass.destDir + '/normalize.css'
+            ])
+            .pipe(gulp.dest(config.drupal.cssDir))
+            .pipe(browserSync.stream())
+        ;
+    }
+    return;
+});
+
+gulp.task('drupal:copy-patterns', function() {
+    if (isDirectory(config.drupal.patternsDir)) {
+        gulp.src(config.patternFiles)
+            .pipe(gulp.dest(config.drupal.patternsDir))
+        ;
+    }
+    return;
+});
+
+gulp.task('drupal:copy-images', function() {
+    if (isDirectory(config.drupal.imagesDir)) {
+        gulp.src(config.imageFiles)
+            .pipe(gulp.dest(config.drupal.imagesDir))
+        ;
+    }
+    return;
+});
+
+gulp.task('drupal:copy', ['drupal:copy-css', 'drupal:copy-patterns', 'drupal:copy-images']);
+
+gulp.task('pl:generate', function() {
+    if (isDirectory(config.patternLab.dir)) {
+        run('php ' + config.patternLab.dir + '/core/console --generate').exec();
+    }
+});
+
+gulp.task('drush:cr', function() {
+    return run('drush ' + config.drush.alias + ' cr').exec();
+});
+
+gulp.task('bs:reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['watch']);
